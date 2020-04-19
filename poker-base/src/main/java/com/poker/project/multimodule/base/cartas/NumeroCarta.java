87dcbd26b85c74e39f4b68c2d3a0313d1462c47a
victor penit <vpen01@ucm.es>
2020-04-19 20:16:32 +0200

    poner el numero de carta como enumerado

---
+++ poker-base/src/main/java/com/poker/project/multimodule/base/cartas/NumeroCarta.java
@@ -0,0 +1,29 @@
+package com.poker.project.multimodule.base.cartas;
+
+public enum NumeroCarta 
+{
+	
+	
+	AS(1),
+	DOS(2),
+	TRES(3),
+	CUATRO(4),
+	CINCO(5),
+	SEIS(6),
+	SIETE(7),
+	OCHO(8),
+	NUEVE(9),
+	DIEZ(10),
+	JACK(11),
+	QUEEN(12),
+	KING(13);
+	
+	int valor;
+
+	private NumeroCarta(int valor) {
+		this.valor = valor;
+	}
+	
+	
+
+}
